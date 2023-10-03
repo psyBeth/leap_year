29084b062cd954bf289b163dcef121e9eb2f8957
Betul Koru <betulkoru06@gmail.com>
2023-10-03 14:24:07 +0300

    first commit

---
+++ condition.js
@@ -0,0 +1,6 @@
+const year = number(prompt("Enter a year: "))
+if ( year % 400 = 0 ) {
+    console.log("${year} is a leap year.")
+} else { 
+    console.log("${year} is not a leap year.")
+} // ? nereye noktalama işareti anlamadım
